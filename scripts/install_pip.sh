c4437d3a986406320fb44210a44c768373bf3de0
harish-prakash-99 <55242672+harish-prakash-99@users.noreply.github.com>
2021-05-13 08:07:59 +0530

    Update install_pip.sh

--- scripts/install_pip.sh
+++ scripts/install_pip.sh
@@ -4,7 +4,7 @@ echo "Creating virtual environment"
 python3.7 -m venv vibe-env
 echo "Activating virtual environment"
 
-source /content/VIBE/vibe-env/bin/activate     #This is specific to Colab, when VIBE is cloned onto the /content directory
+source VIBE/vibe-env/bin/activate     #This is specific to Colab, when VIBE is cloned onto the /content directory
 
 pip install numpy==1.17.5 torch==1.4.0 torchvision==0.5.0
 pip install git+https://github.com/giacaglia/pytube.git --upgrade
